85c06792596635c301b2d9ed29089d5dde33a867
Pashoshdrag@gmail.com <Pashoshdrag@gmail.com@c3d73ca3-f625-8484-2e3d-09e4e5e273d3>
2013-08-20 18:11:58 +0000

    main window creation

---
+++ src/GUI/Lectur_Preferences.java
@@ -0,0 +1,87 @@
+package GUI;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.Dimension;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import javax.swing.border.BevelBorder;
+import java.awt.Rectangle;
+import java.awt.Font;
+import java.awt.Component;
+import java.awt.Point;
+import javax.swing.SwingConstants;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.Window.Type;
+import java.util.Locale;
+import java.awt.SystemColor;
+import java.awt.Color;
+import javax.swing.UIManager;
+
+public class Lectur_Preferences {
+
+	private JFrame frmLec;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Lectur_Preferences window = new Lectur_Preferences();
+					window.frmLec.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Lectur_Preferences() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmLec = new JFrame();
+		frmLec.getContentPane().setBackground(SystemColor.menu);
+		frmLec.setBackground(SystemColor.inactiveCaptionBorder);
+		frmLec.setForeground(SystemColor.controlLtHighlight);
+		frmLec.setLocale(new Locale("en", "IL"));
+		frmLec.setType(Type.UTILITY);
+		frmLec.setResizable(false);
+		frmLec.setAlwaysOnTop(true);
+		frmLec.setPreferredSize(new Dimension(800, 600));
+		frmLec.setName("Main_Panel");
+		frmLec.setTitle("Time table schedualing system");
+		frmLec.setMinimumSize(new Dimension(800, 600));
+		frmLec.setMaximumSize(new Dimension(800, 600));
+		frmLec.setBounds(100, 100, 450, 300);
+		frmLec.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmLec.getContentPane().setLayout(null);
+		
+		JLabel lblTimeTableSchedualing = new JLabel("Time Table Schedualing System");
+		lblTimeTableSchedualing.setHorizontalTextPosition(SwingConstants.CENTER);
+		lblTimeTableSchedualing.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTimeTableSchedualing.setLocation(new Point(50, 0));
+		lblTimeTableSchedualing.setAlignmentX(Component.CENTER_ALIGNMENT);
+		lblTimeTableSchedualing.setFont(new Font("Tahoma", Font.BOLD, 36));
+		lblTimeTableSchedualing.setBounds(new Rectangle(10, 11, 774, 64));
+		lblTimeTableSchedualing.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+		frmLec.getContentPane().add(lblTimeTableSchedualing);
+		
+		JPanel PNL_Main = new JPanel();
+		PNL_Main.setMinimumSize(new Dimension(774, 474));
+		PNL_Main.setMaximumSize(new Dimension(774, 474));
+		PNL_Main.setBounds(10, 86, 774, 474);
+		frmLec.getContentPane().add(PNL_Main);
+	}
+}
